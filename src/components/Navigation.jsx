7d8d0be58c998f6c665709409315090ed07bc414
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2021-09-09 15:04:39 +0200

    Responsive NavBar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,33 @@
-import React from "react";
-import {Link, withRouter} from "react-router-dom";
-
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 import logo_accrobatie from "../images/logo-accrobatie.png";
 
 function Navigation (props) {
+
+    const [toggleMenu, setToggleMenu ] = useState(false);
+    const [ largeur, setLargeur ] = useState(window.innerWidth);
+
+    const toggleNavSmallScreen = () => {
+        setToggleMenu(!toggleMenu);
+    }
+
+    useEffect( () => {
+
+        const changeWidth = () => {
+            setLargeur(window.innerWidth);
+
+            if (window.innerWidth > 700) {
+                setToggleMenu(false);
+            }
+        }
+
+        window.addEventListener("resize", changeWidth);
+
+        return () => {
+            window.removeEventListener("resize", changeWidth);
+        }
+    }, [])
+
     return (
         <div className="navigation">
 
@@ -14,31 +38,37 @@ function Navigation (props) {
             </div>
 
             <nav className="navbar">
-                <ul className="navigation-list">
-                    <li className="navigation-list-element">
-                        <Link to="/about">
-                            Qui sommes-nous ?
-                        </Link>
-                    </li>
-                    |
-                    <li className="navigation-list-element">
-                        <Link to="/routes">
-                            Nos parcours
-                        </Link>
-                    </li>
-                    |
-                    <li className="navigation-list-element">
-                        <Link to="/activities">
-                            Nos services et activités
-                        </Link>
-                    </li>
-                    |
-                    <li className="navigation-list-element">
-                        <Link to="/contact">
-                            Contact et reservation
-                        </Link>
-                    </li>
-                </ul>
+                {(toggleMenu || largeur > 700) &&
+                (
+                    <ul className="navigation-list">
+                        <li className="navigation-list-element">
+                            <Link className="nav-link" to="/about">
+                                Qui sommes-nous ?
+                            </Link>
+                        </li>
+
+                        <li className="navigation-list-element">
+                            <Link className="nav-link" to="/routes">
+                                Nos parcours
+                            </Link>
+                        </li>
+
+                        <li className="navigation-list-element">
+                            <Link className="nav-link" to="/activities">
+                                Nos services et activités
+                            </Link>
+                        </li>
+
+                        <li className="navigation-list-element">
+                            <Link className="nav-link" to="/contact">
+                                Contact et reservation
+                            </Link>
+                        </li>
+                    </ul>
+                )}
+                <div className="navigation-button-container">
+                    <button onClick={toggleNavSmallScreen} className="navigation-button">MENU</button>
+                </div>
             </nav>
 
         </div>
