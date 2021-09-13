3e5612c619ce232bae3a20aef2ef6730ea060894
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2021-09-13 21:12:59 +0200

    Structure de base Routes.jsx

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -5,6 +5,58 @@ class Routes extends Component {
         return (
             <div className="content">
                 <h1>Nos parcours</h1>
+
+
+                <div className="route-container">
+
+                    {/* Début de l'élément */}
+                    <div className="yellow-route">
+                        <div className="route-detail">
+                            <h1>Noob</h1>
+                            <h2>Parcours adpaté à tous, parce qu'il faut bien commencer par quelques part 😉</h2>
+                        </div>
+                    </div>
+                    {/* Fin de l'élément */}
+
+                    {/* Début de l'élément */}
+                    <div className="green-route">
+                        <div className="route-detail">
+                            <h1>Ez</h1>
+                            <h2>Tu veux pas passer pour un débutant, dis le ! 😏</h2>
+                        </div>
+                    </div>
+                    {/* Fin de l'élément */}
+
+                    {/* Début de l'élément */}
+                    <div className="blue-route">
+                        <div className="route-detail">
+                            <h1>Pro</h1>
+                            <h2>Les choses sérieuses commencent ! 💪</h2>
+                        </div>
+                    </div>
+                    {/* Fin de l'élément */}
+
+                    {/* Début de l'élément */}
+                    <div className="purple-route">
+                        <div className="route-detail">
+                            <h1>MVP</h1>
+                            <h2>T'es le boss de l'accro'batie ! 👑</h2>
+                        </div>
+                    </div>
+                    {/* Fin de l'élément */}
+
+                    {/* Début de l'élément */}
+                    <div className="red-route">
+                        <div className="route-detail">
+                            <h1>Godlike</h1>
+                            <h2>T'es pas huamin toi, t'es différent ! 🔥</h2>
+                        </div>
+                    </div>
+                    {/* Fin de l'élément */}
+
+                </div>
+
+
             </div>
         );
     }
