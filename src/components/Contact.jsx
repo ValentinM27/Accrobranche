d68938f39e54fad784f230015411239817d0fb09
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2021-09-19 13:48:33 +0200

    Création du style du formulaire

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import NumericInput from 'react-numeric-input';
+import {Link} from "react-router-dom";
 
 class Contact extends Component {
+
     render() {
         return (
             <div className="content">
@@ -11,26 +13,52 @@ class Contact extends Component {
                     <fieldset>
                         <form action="mailto:Valentin.Marguerie@iut-dijon.u-bourgogne.fr" encType="text/plain" method="post" className="contact-form">
                             <div className="contact-form-radio">
-                                <input type="radio" id="Reservation" name="Reservation" value="Reservation" checked />
+                                <input type="radio" id="Reservation" name="Demande" value="Reservation" checked/>
                                 <label htmlFor="Reservation">Réservation</label>
-                                <input type="radio" id="Demande" name="Reservation" value="Demande" />
-                                <label htmlFor="Demande">Demande d'information</label>
+                                <input type="radio" id="Demande" name="Demande" value="Demande" />
+                                <label htmlFor="Demande">Demande d'informations</label>
                             </div>
 
                             <div className="contact-form-info">
-                                <input type="text" placeholder="prénom" name="firstname" id="lastname" required/>
-                                <input type="text" placeholder="nom" name="lastname" id="lastname" required/>
+                                <select id="contact-form-gender">
+                                    <option value="Male">M</option>
+                                    <option value="Female">Mme</option>
+                                    <option value="Other">Autres</option>
+                                </select>
+
+                                <input type="text" placeholder="Prénom" name="Firstname" id="lastname" required/>
+                                <input type="text" placeholder="Nom" name="Lastname" id="lastname" required/>
                             </div>
 
-                            <label htmlFor="Age">Votre âge</label>
-                            <NumericInput min={7} max={130} value={30} />
+                            <div className="contact-form-date">
+                                <label htmlFor="Age">Votre âge</label>
+                                <NumericInput name="Age" min={7} max={130} value={30} />
 
-                            <div className="contact-form-text">
-                                <label htmlFor="informations">Informations supplémentaires (Nombre de personnes, enfants, parcours choisi ... </label>
-                                <textarea></textarea>
+                                <div>
+                                    <label htmlFor="start">Date de début:</label>
+                                    <input type="date" id="start" name="trip-start"/>
+
+                                    <label htmlFor="start">Date de fin:</label>
+                                    <input type="date" id="end" name="trip-end"/>
+                                </div>
+
                             </div>
 
-                            <input type="submit"/>
+
+                            <textarea placeholder="Informations supplémentaires (Nombre de personnes, enfants, parcours choisi ... )" name="informations"
+                            id="contact-form-comment" cols="40" aria-autocomplete="list" aria-haspopup="true"/>
+
+                            <div className="contact-form-confidential-container">
+                                <input type="checkbox" id="termes" name="termes" required/>
+                                <label  htmlFor="termes">J'affirme avoir pris connaisance de la
+                                    <Link className="contact-form-confidential " to="/confidential">
+                                        politique de confidentialité
+                                    </Link>
+                                </label>
+                            </div>
+
+
+                            <input className="contact-form-submit" type="submit"/>
                         </form>
                     </fieldset>
                 </div>
