c6bb8606962610cfaddd553d44cc1cc5876ff53c
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2021-10-17 17:24:15 +0200

    Tableau des pass

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -20,6 +20,54 @@ class Activities extends Component {
                             <p>{pass.description}</p>
                         </div>
                     ))}
+
+                    <div className="table-wrapper">
+                        <table className="fl-table">
+                            <thead>
+                                <tr>
+                                    <th>Pass</th>
+                                    <th>-6 ans</th>
+                                    <th>6-12 ans</th>
+                                    <th>12-18 ans</th>
+                                    <th>Plein tarif</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Pass'age</td>
+                                    <td>*</td>
+                                    <td>4€</td>
+                                    <td>6€</td>
+                                    <td>8€</td>
+                                </tr>
+                                <tr>
+                                    <td>Im'pass</td>
+                                    <td>*</td>
+                                    <td>8€</td>
+                                    <td>12€</td>
+                                    <td>16€</td>
+                                </tr>
+                                <tr>
+                                    <td>Pass'taga</td>
+                                    <td>6€</td>
+                                    <td>16€</td>
+                                    <td>22€</td>
+                                    <td>28€</td>
+                                </tr>
+                                <tr>
+                                    <td>Pass'partout</td>
+                                    <td>6€</td>
+                                    <td>19€</td>
+                                    <td>26€</td>
+                                    <td>32€</td>
+                                </tr>
+                                <tr>
+                                    <td>Pass'port</td>
+                                    <td colSpan="4" align="center">Tarif Pass'Partout + Prix de la cabanne choisi</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
             </div>
